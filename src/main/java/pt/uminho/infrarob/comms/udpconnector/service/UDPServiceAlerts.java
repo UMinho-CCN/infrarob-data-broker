c6653a926077941224237cb7ecd414f2e74f0e90
Fábio Gonçalves <b7207@algoritmi.uminho.pt>
2024-08-23 01:05:50 +0100

    Closes #29
    received alert in json format and converts to java object

--- src/main/java/pt/uminho/infrarob/comms/udpconnector/service/UDPServiceAlerts.java
+++ src/main/java/pt/uminho/infrarob/comms/udpconnector/service/UDPServiceAlerts.java
@@ -1,5 +1,7 @@
 package pt.uminho.infrarob.comms.udpconnector.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.messaging.Message;
@@ -8,6 +10,7 @@ import pt.uminho.infrarob.common.objects.enums.InfractionType;
 import pt.uminho.infrarob.common.objects.internal.InternalData;
 import pt.uminho.infrarob.common.objects.internal.InternalEventData;
 import pt.uminho.infrarob.common.objects.internal.InternalObjectData;
+import pt.uminho.infrarob.common.objects.proto1.Proto1Alert;
 import pt.uminho.infrarob.events.events.V2XMessageReceivedEvent;
 import pt.uminho.infrarob.events.events.V2xMessageOutputEvent;
 
@@ -19,18 +22,34 @@ public class UDPServiceAlerts {
     private long ID = 0;
     public void receive(Message message){
         String data = new String((byte[]) message.getPayload());
-        String posData[] = data.split(";");
         InternalData internalObjectData = new InternalData();
+
+        /*
+        String posData[] = data.split(";");
+
         InternalEventData eventData = new InternalEventData();
         eventData.setLat(0);
         eventData.setLon(0);
         eventData.setEventID(0);
         eventData.setOrigin("RPAS");
         eventData.setEventType(InfractionType.SAFEZONE_INFRACION);
-        internalObjectData.addEvents(eventData);
 
-        V2xMessageOutputEvent outputEvent = new V2xMessageOutputEvent(this, internalObjectData);
+         */
+        ObjectMapper mapper = new ObjectMapper();
+        Proto1Alert proto1Alert = null;
+        try {
+            proto1Alert = mapper.readValue(data, Proto1Alert.class);
+            InternalEventData eventData = proto1Alert.toInternalEventDatA();
+            eventData.setOrigin("RPAS");
+            internalObjectData.addEvents(eventData);
 
-        applicationEventPublisher.publishEvent(outputEvent);
+            V2xMessageOutputEvent outputEvent = new V2xMessageOutputEvent(this, internalObjectData);
+
+            applicationEventPublisher.publishEvent(outputEvent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
